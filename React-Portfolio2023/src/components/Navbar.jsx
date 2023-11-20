4d89cda54eb85d329e3cb0a9ee6a719069d6db2c
Sara Garcia <saragca81@gmail.com>
2023-11-20 13:04:30 -0800

    updated files

---
+++ React-Portfolio2023/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import '../styles/Navbar.css';
+
+const styles = {
+    navbarStyle: {
+        background: '',
+        justifyContent: 'flex-end',
+    },
+}
+
+function Navbar() {
+    return (
+        <nav style={style.navbarStyle} className='navbar'>
+            <a href="/">ABOUT ME</a>
+            <a href="/">PORTFOLIO</a>
+            <a href="/">CONTACT</a>
+            <a href="/">RESUME</a>
+        </nav>
+    );
+}
+export default Navbar;
