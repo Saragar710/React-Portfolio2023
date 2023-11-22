07fcc994793d73c7b36d2e64b42e9ad99db3715c
Sara Garcia <saragca81@gmail.com>
2023-11-21 17:17:26 -0800

    added code to Portfolio jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,36 @@
 import '../styles/Portfolio.css'
 
+
+
+const styles = {
+    PortfolioStyles: {
+        background: 'C297B8'
+    }
+}
 const Portfolio= ()  =>{
     return (
-        <div> 
+        <>
+         <div id="" class="bottom-left">
+                    <h1 class="Projects">Employee Tracker</h1>
+                </div>
+                <div id="Forest" class="bottom-left">
+                    <h1 class="Projects">Express Note Taker</h1>
+                </div>
+                <div id="Forest" class="bottom-left">
+                    <h1 class="Projects">Weather Forecast</h1>
+                </div>
+                <div id="Forest" class="bottom-left">
+                    <h1 class="Projects">clubBook</h1>
+                </div>
+                
+        
+        
+        </>
+
+    
             
-        </div>
+    
+
     );
 }
 export default Portfolio;
