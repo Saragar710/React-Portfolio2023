9c4f5da849c1a80c314e14e53fe68bf6d11e6a3e
Sara Garcia <saragca81@gmail.com>
2023-11-25 13:46:31 -0800

    added links to profilejsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,17 +10,35 @@ const styles = {
 const Portfolio= ()  =>{
     return (
         <>
-         <div id="" class="bottom-left">
-                    <h1 class="Projects">Employee Tracker</h1>
+         <div className="container">
+                    <h1 className="Projects">Employee Tracker</h1>
+                    <img
+                    className="Employee Tracker"
+                    src={`https://github.com/Saragar710/Employee-Trackerv1`}
+                    alt="Empoyee Tracker"
+                    />
                 </div>
-                <div id="Forest" class="bottom-left">
-                    <h1 class="Projects">Express Note Taker</h1>
+                <div className="conatiner">
+                    <h1 className="Projects">Express Note Taker</h1>
+                    <img
+                    className="Express Note Taker"
+                    src={`https://github.com/Saragar710/Express-Note-Taker092023 `}
+                    alt="Express Note Taker"
+                    />
                 </div>
-                <div id="Forest" class="bottom-left">
-                    <h1 class="Projects">Weather Forecast</h1>
+                <div className="container">
+                    <h1 className="Projects">Weather Forecast</h1>
+                    <img 
+                    className='Weather Forecast'
+                    src={` https://github.com/Saragar710/Weather-Forecast-For-Aug-2023`}
+                    alt="Weather Forecast"/>
                 </div>
-                <div id="Forest" class="bottom-left">
-                    <h1 class="Projects">clubBook</h1>
+                <div className='clubBook'>
+                    <h1 className="Projects">clubBook</h1>
+                    <img 
+                    src={`https://github.com/Saragar710/ClubBook`}
+                    alt="clubBook"
+                    />
                 </div>
                 
         
