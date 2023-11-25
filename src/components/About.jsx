719191c7b217bde6197b7a0551269265377e8694
Sara Garcia <saragca81@gmail.com>
2023-11-25 13:57:16 -0800

    added Raven pic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,12 @@ import './styles/About.css'
 const About =() => {
     return (
         <div> 
+            <div className='container'
+        <img
+            src={` "C:\Users\Sarag\OneDrive\Pictures\Saved Pictures\Raven.png"`}
+            />
             <p>
-                I am a Front End Webdeveloper. 
+                I am a Front End Webdeveloper. I am excited to continue to learn and grow in my new career path.
             </p>
         </div>
     );
