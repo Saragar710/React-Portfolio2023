193b1654ed303c2061a5c8174e02159d3f508947
Sara Garcia <saragca81@gmail.com>
2023-11-26 14:35:21 -0800

    updated paths. changed back error main

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-// import './styles/About.css';
-// import '../assets/Raven.jpg';
-// // 
-// console.log(Raven);
+ //import './styles/About.css';
+ //import '../assets/Raven.jpg';
+
+//console.log(Raven);
 
 function About() {
     return (
     <div>
-            {/* <img src={Raven} alt="Raven" /> */}
+             {/*<img src={Raven} alt="Raven" />*/ }
 
             <p>
-                I am a Front End Webdeveloper. I am excited to continue to learn and grow in my new career path.
+               My name is Sara Garcia. I am a chef, a mother and a wife. I currently work as a Food and Beverage Supervisor of two hotels. As of right now, I run Breakfast for both. I am in charge of Ordering and organizing the food that is delivered to the hotels. I wrote the guidlines for our kitchens in regards to food saftey procedures as well as training all my coworkers. As our hotels grow, we are doing more and more banquets and events. I help prepare the food, setup the conference rooms, deliver and maintain the food served then clean up.  With the help of my amazing coworkers, I am learning spanish. My coworkers are amazing, and all though I like my job, I want more. That is why I decided to go back to school and learn something I have always found facinating. With a few lines of code, you can create an app or a website! Having Stanley Lewis as my instructor was phenomenal. He is so passionate about this industry it is hard to keep up! He has an amazing team. I have worked with alot of different tutors, but Sandy Smith was my absolute favorite. Everyone on this team has been wonderful and supportive. I could not have chosen a better team if I had the chance!
             </p>
             <p>
-                summary
+                Although I am very new to this industry, I can not wait to dive in and keep learning. It is incredible to think of all the fun things you can do. I want to learn everything and understand why and how it works. I want to make the websites come to life with CSS and have them be responsive with JavaScript. 
             </p>
             <p>
-                why you should hire me
+                As I said before, I am new to this industry. BUT I will not give up. Im sure there will be days filled with frustration, however, the words "I give up" are not in my vocabulary. When you hire me you will soon find out I will be one of the hardest workers on your team. You can depend on me to get the job done on time. 
             </p>
         </div>
     );
