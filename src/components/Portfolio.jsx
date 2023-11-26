86b0f5cffdc21497fa5941124bba20a3b13496dc
Sara Garcia <saragca81@gmail.com>
2023-11-26 12:59:36 -0800

    added images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -16,8 +16,27 @@ function Portfolio() {
             deploy:"/https://express-note-taker092023-fb3cb27dd87e.herokuapp.com/ ",
             repo: "https://github.com/Saragar710/Express-Note-Taker092023 "
         },
+        {
+            name: 'Employee_Tracker',
+            deploy: "https://drive.google.com/file/d/1VWfWTgZ9kW4hP9m9q9xHJLXuq3mmawa-/view",
+            repo: "https://github.com/Saragar710/Employee-Trackerv1"
+        },
+        {
+            name: 'Weather_Forecast',
+            deploy: "https://saragar710.github.io/Weather-Forecast-For-Aug-2023",
+            repo: "htts://github.com/Saragar710/Weather-Forecast-For-Aug-2023"
+        },
+        {
+            name: 'clubBook',
+            deploy: "https://club-book-688a8712a651.herokuapp.com",
+            repo: "https://github.com/Saragar710/ClubBook"
+        },
+        {
+            name: 'Travel_Itinerary',
+            deploy:"https://saragar710.github.io/Travel-Itinerary ",
+            repo:"https://github.com/Saragar710/Travel-Itinerary"
+        }
     ])
-
     return (
         <>
         <div className='flex-row'>
@@ -34,36 +53,3 @@ function Portfolio() {
 
 export default Portfolio;
 
-{/* <div className="container">
-<h1 className="Projects">Employee Tracker</h1>
-<img
-className="Employee-Tracker"
-src={`https://github.com/Saragar710/Employee-Trackerv1`}
-alt="Empoyee Tracker"
-/>
-</div>
-<div className="conatiner">
-<h1 className="Projects">Express Note Taker</h1>
-<img
-className="Express-Note-Taker"
-src={`https://github.com/Saragar710/Express-Note-Taker092023 `}
-//https://express-note-taker092023-fb3cb27dd87e.herokuapp.com/
-alt="Express Note Taker"
-/>
-</div>
-<div className="container">
-<h1 className="Projects">Weather Forecast</h1>
-<img 
-className='Weather-Forecast'
-src={` https://github.com/Saragar710/Weather-Forecast-For-Aug-2023`}
-//https://saragar710.github.io/Weather-Forecast-For-Aug-2023/
-alt="Weather Forecast"/>
-</div>
-<div className='clubBook'>
-<h1 className="Projects">clubBook</h1>
-<img 
-src={`https://github.com/Saragar710/ClubBook`}
-//https://club-book-688a8712a651.herokuapp.com/
-alt="clubBook"
-/>
-</div> */}
