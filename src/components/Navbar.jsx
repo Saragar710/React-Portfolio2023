5dcc965e57d5303971b147641cc8223b5e503591
Sara Garcia <saragca81@gmail.com>
2023-11-30 13:46:30 -0800

    updated path in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,53 @@
-import { Link} from 'react-router-dom';
+// import { Link} from 'react-router-dom';
+// import '../styles/Navbar.css';
+
+
+// export default function Navbar({currentPage}) {
+//     // const linkStyle = { border: '1px solid black', padding: '5px'};
+//    //const currentPage= window.location.pathname
+// // console.log(currentPath)
+//   const pages = ['portfolio', 'contact', 'resume'];
+
+//     return (
+// <nav>
+//   <ul className='flex-row'>
+//     { currentPage =="/"? (pages):(
+//        <li className={`mx-5 ${currentPage === '/'  && 'navActive'}`} key="about">
+//       <Link to="/"></Link>
+//     </li>)}
+  
+//       {pages.map((Page) => ( 
+//        <li className={`mx-5 ${currentPage === `/${Page}` && 'navActive'}`} key={Page} >
+//         <Link to={`/${Page}`}></Link>
+//      </li>
+//      ))
+//       }
+//   </ul>
+// </nav>
+//     );
+//   }
+    
+import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 
-
-export default function Navbar({currentPage}) {
-    // const linkStyle = { border: '1px solid black', padding: '5px'};
-   //const currentPage= window.location.pathname
-// console.log(currentPath)
+export default function Navbar({ currentPage }) {
   const pages = ['portfolio', 'contact', 'resume'];
 
-    return (
-<nav>
-  <ul className='flex-row'>
-    { currentPage =="/"? (pages):(
-       <li className={`mx-5 ${currentPage === '/'  && 'navActive'}`} key="about">
-      <Link to="">About</Link>
-    </li>)}
-   { /*// <li className={`mx-5 ${currentPage === '/'  && 'navActive'}`} key="about">
-    //   <Link to="/">About</Link>
-    // </li> */}
-    {/* <li>
-      <Link to="/">Portfolio</Link>
-    </li>
-   <li>
-    <Link to="/">Contact</Link>
-   </li>
-   <li>
-    <Link to="">Resume</Link>
-   </li> */}
-      {pages.map((Page) => ( 
-       <li className={`mx-5 ${currentPage === `/${Page}` && 'navActive'}`} key={Page} >
-        <Link to={`/${Page}`}></Link>
-     </li>
-     ))
-      }
-  </ul>
-</nav>
-    );
-  }
-    
+  return (
+    <nav>
+      <ul className='flex-row'>
+        {currentPage === '/' ? (
+          <li className={`mx-5 ${currentPage === '/' && 'navActive'}`} key="about">
+            <Link to="/">About</Link>
+          </li>
+        ) : null}
+
+        {pages.map((page) => (
+          <li className={`mx-5 ${currentPage === `/${page}` && 'navActive'}`} key={page}>
+            <Link to={`/${page}`}>{page}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
